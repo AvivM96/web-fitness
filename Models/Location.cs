f4c88c062736de8042a8bd0810562a8145a8745a
Gilsemo <gilsemo23@gmail.com>
2020-05-16 18:25:15 +0300

    wip

---
+++ Models/Location.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+
+namespace web_fitness.Models
+{
+    public class Location
+    {
+        [JsonProperty("lat")]
+        public string lat { get; set; }
+
+        [JsonProperty("lon")]
+        public string lon { get; set; }
+
+    }
+}
---
+++ Models/Trainers.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace web_fitness.Models
+{
+    public class Trainers
+    {
+
+        [Key]
+        [ScaffoldColumn(false)]
+        public int TrainerId { get; set; }
+
+
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        [Display(Name = "Trainer Mail")]
+        public string Mail { get; set; }
+
+        [Required]
+        [Display(Name = "Trainer Name")]
+        public string TrainerName { get; set; }
+
+        [Display(Name = "Trainer Phone")]
+        public string TrainerPhone { get; set; }
+
+        [Display(Name = "Trainer Gender")]
+        public string TrainerGender { get; set; }
+
+        [Display(Name = "Trainer Address")]
+        [StringLength(60, MinimumLength = 5)]
+        [Required]
+        public string Address { get; set; }
+
+        [Display(Name = "Trainer City")]
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
+        public string City { get; set; }
+
+
+        public ICollection<Meetings> Meeting { get; set; }
+
+
+
+
+
+    }
+}
