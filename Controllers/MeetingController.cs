fac40a3c6125072bab16ae3e54e8170a6f462a5f
Aviv Moshe <avivmoshe96@gmail.com>
2020-06-08 17:45:49 +0300

    working home screen

---
+++ Controllers/MeetingController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace web_fitness.Controllers
+{
+    public class MeetingController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index(string eventTypeName)
+        {
+            ViewData["eventTypeName"] = eventTypeName;
+            return View();
+        }
+    }
+}
---
+++ Controllers/TrainerController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using web_fitness.Data;
+using web_fitness.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace web_fitness.Controllers
+{
+    public class TrainerController : Controller
+    {
+
+        private readonly fitnessdataContext _context;
+
+        public TrainerController(fitnessdataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            Console.WriteLine();
+            return View();
+        }
+
+        public async Task<ActionResult> Details(string id)
+        {
+            ViewData["test"] = id;
+            var trainer = await _context.Trainers.SingleOrDefaultAsync(t => t.TrainerName == "Aviv Test");
+            return View(trainer);
+        }
+    }
+}
+
