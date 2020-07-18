ec8edc0b00b17c635b799bcd84e222e363a2731d
Gilsemo <gilsemo23@gmail.com>
2020-07-18 14:36:45 +0300

    add graphs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,20 +6,28 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using web_fitness.Models;
+using web_fitness.Data;
+using web_fitness.Models;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
 
 namespace web_fitness.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly fitnessdataContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(fitnessdataContext context)
         {
-            _logger = logger;
-        }
+            _context = context;
 
+        }
+        public IActionResult Graphs()
+        {
+            return View();
+        }
         public IActionResult Index()
         {
+
             return View();
         }
 
@@ -27,11 +35,14 @@ namespace web_fitness.Controllers
         {
             return View();
         }
+     
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
     }
 }
